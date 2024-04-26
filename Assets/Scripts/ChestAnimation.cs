c9198e9afa68bf26bb35233e105b072ce3630d75
NathanHaigh <nathan2.haigh@live.uwe.ac.uk>
2024-04-26 14:16:47 +0100

    Opening and Closing Chest

---
+++ Assets/Scripts/ChestAnimation.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestAnimation : MonoBehaviour
+{
+
+    public Animator animator;
+    bool InTriggerRange = false;
+    bool open = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(InTriggerRange == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && open == false)
+            {
+                open = true;
+                print("Hello");
+                animator.SetTrigger("OpenTrigger");
+            }
+
+            else if(open == true)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    open = false;
+                    print("Closing");
+                    animator.ResetTrigger("OpenTrigger");
+                }
+            }
+        }       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            print("Inside");
+            InTriggerRange = true;
+        }
+    }
+}
